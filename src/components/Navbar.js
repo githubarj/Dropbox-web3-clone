b653dd9210a7e3ac771ea41152706dbddfa734d2
Richard Jeremy <githuba9520@gmail.com>
2022-09-16 00:29:12 +0300

    functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
-import Identicon from "identicon.js";
-import box from "../box.png";
+import React, { Component } from 'react';
+import Identicon from 'identicon.js';
+import box from '../box.png'
 
 class Navbar extends Component {
+
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark p-0 text-monospace">
@@ -18,31 +19,24 @@ class Navbar extends Component {
         <ul className="navbar-nav px-3">
           <li>
             <small id="account">
-              <a
-                target="_blank"
-                alt=""
-                className="text-white"
-                rel="noopener noreferrer"
-                href={"https://etherscan.io/address/" + this.props.account}
-              >
-                {this.props.account.substring(0, 6)}...
-                {this.props.account.substring(38, 42)}
+              <a target="_blank"
+                 alt=""
+                 className="text-white"
+                 rel="noopener noreferrer"
+                 href={"https://etherscan.io/address/" + this.props.account}>
+                {this.props.account.substring(0,6)}...{this.props.account.substring(38,42)}
               </a>
             </small>
-            {this.props.account ? (
-              <img
-                alt=""
-                className="ml-2"
-                width="30"
-                height="30"
-                src={`data:image/png;base64,${new Identicon(
-                  this.props.account,
-                  30
-                ).toString()}`}
-              />
-            ) : (
-              <span></span>
-            )}
+            { this.props.account
+              ? <img
+                  alt=""
+                  className='ml-2'
+                  width='30'
+                  height='30'
+                  src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
+                />
+              : <span></span>
+            }
           </li>
         </ul>
       </nav>
